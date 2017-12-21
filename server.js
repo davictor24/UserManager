20f4adc05183715e619b39b13e60c23714aeb63a
Victor Durojaiye <durojaiyevic@gmail.com>
2017-12-21 12:08:46 +0100

    Bound server.js to IP 0.0.0.0

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 var app = require('./app');
 var port = process.env.PORT;
 
-var server = app.listen(port, function() {
+var server = app.listen(port, '0.0.0.0', function() {
   console.log('Express server listening on port ' + port);
 });
